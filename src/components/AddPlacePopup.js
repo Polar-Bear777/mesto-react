f7329e0322342e1a07748c0fd31654055f2a65fc
Vladislav Alkhimenko <stormyaaa@yandex.ru>
2023-04-01 11:48:25 +0800

    2 popytka

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -1,66 +1,68 @@
 import React from "react";
 import PopupWithForm from "./PopupWithForm";
 
-function AddPlacePopup({ onAddPlace, isOpen, onClose}) {
-    // Стейты значений
-    const [name, setName] = React.useState('');
-    const [link, setLink] = React.useState('');
+function AddPlacePopup({ onAddPlace, isOpen, onClose }) {
+  // Стейты значений
+  const [name, setName] = React.useState("");
+  const [link, setLink] = React.useState("");
 
-    function handleNameChange(e) {
-        setName(e.target.value);
-    }
-  
-    function handleLinkChange(e) {
-        setLink(e.target.value);
-    }
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        onAddPlace({
-          name: name,
-          link: link,
-        });
-    }
+  function handleLinkChange(e) {
+    setLink(e.target.value);
+  }
 
-    // Сбросим форму при открытии
-    React.useEffect(() => {
-      setName('');
-      setLink('');
-    }, [isOpen]); 
+  function handleSubmit(e) {
+    e.preventDefault();
+    onAddPlace({
+      name: name,
+      link: link,
+    });
+  }
 
-    return(
-        <PopupWithForm 
-          name="card" 
-          title="Новое место"
-          btnText="Сохранить" 
-          isOpen={isOpen} 
-          onClose={onClose}
-          onSubmit={handleSubmit}
-        >
-          <input
-            type="text"
-            id="title"
-            className="popup__input popup-card__input popup-card__input_type_title"
-            name="name"
-            placeholder="Название"
-            minLength={2}
-            maxLength={30}
-            required
-            onChange={handleNameChange}
-          />
-          <span id="title-error" className="error error_visible" />
-          <input
-            type="url"
-            id="link"
-            className="popup__input popup-card__input popup-card__input_type_link"
-            name="link"
-            placeholder="Ссылка на картинку"
-            required
-            onChange={handleLinkChange}
-          />
-          <span id="link-error" className="error error_visible" />
-        </PopupWithForm>
-    )
+  // Сбросим форму при открытии
+  React.useEffect(() => {
+    setName("");
+    setLink("");
+  }, [isOpen]);
+
+  return (
+    <PopupWithForm
+      name="card"
+      title="Новое место"
+      btnText="Сохранить"
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmit={handleSubmit}
+    >
+      <input
+        type="text"
+        id="title"
+        className="popup__input popup-card__input popup-card__input_type_title"
+        name="name"
+        placeholder="Название"
+        minLength={2}
+        maxLength={30}
+        required
+        onChange={handleNameChange}
+        value={name || ""}
+      />
+      <span id="title-error" className="error error_visible" />
+      <input
+        type="url"
+        id="link"
+        className="popup__input popup-card__input popup-card__input_type_link"
+        name="link"
+        placeholder="Ссылка на картинку"
+        required
+        onChange={handleLinkChange}
+        value={link || ""}
+      />
+      <span id="link-error" className="error error_visible" />
+    </PopupWithForm>
+  );
 }
 
 export default AddPlacePopup;
